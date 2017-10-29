94c940ae3488414540c64b94ccfd9abd2a258c69
Daniel Nass <dnlnss@gmail.com>
2017-10-29 17:03:32 -0400

    Use NumericInput component

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react';
+import NumericInput from 'react-numeric-input';
 import './Slider.css';
 
 class Slider extends Component {
   constructor(props) {
     super(props);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.clampAndRound = this.clampAndRound.bind(this);
+    this.handleSliderChange = this.handleSliderChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
   }
 
-  handleInputChange(event) {
+  handleSliderChange(event) {
     const name = event.target.name;
     const value = +event.target.value;
 
-    this.props.onChange({name, value});
+    this.props.onChange({ name, value });
   }
 
-  clampAndRound(num) {
-    if (num < this.props.min) return this.props.min;
-    if (num > this.props.max) return this.props.max;
-    return parseFloat(num).toFixed(2);
+  handleTextChange(value, _, input) {
+    const name = input.name;
+    this.props.onChange({ name, value });
   }
 
   render() {
     return (
       <div className='slider'>
         <span className='label'>{this.props.property}</span>
-        <input type='range' name={this.props.property} min={this.props.min} max={this.props.max} step={this.props.step || 0.01} value={this.props.value} onChange={this.handleInputChange}></input>
-        <input type='text' name={this.props.property} value={this.clampAndRound(this.props.value)} onChange={this.handleInputChange}></input>
+        <input type='range' name={this.props.property} min={this.props.min} max={this.props.max} step={this.props.step || 0.01} value={this.props.value} onChange={this.handleSliderChange}></input>
+        <NumericInput min={this.props.min} max={this.props.max} step={this.props.step || 0.01} precision={2} name={this.props.property} value={this.props.value} onChange={this.handleTextChange}></NumericInput>
       </div>
     );
   }
